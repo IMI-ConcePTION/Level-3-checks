1be5f7469bd7b847d4604ee299fa3e1f89dc3b75
Vjola Hoxhaj <vjolahoxhaj19@gmail.com>
2021-08-26 12:32:32 +0200

    Added instructions for birth registry and lifestyle factors completion

--- Level_3_to_be_deployed1.0/to_run.R
+++ Level_3_to_be_deployed1.0/to_run.R
@@ -5,30 +5,86 @@ library(rstudioapi)
 projectFolder<-dirname(rstudioapi::getSourceEditorContext()$path)
 setwd(projectFolder)
 
-###################################################
-#Parameters
-#################################################
-#females age
-min_age_preg<-12
-max_age_preg<-55
-#Set parameters basic parameters
-start_study_date <- "19950101"
-end_study_date <- "20211231"
-lookback_period <- 365
-Age_max <- 56
-Age_min <- 0
-######################################################################################################
+########################################################################################################################
 #Specify all meanings that refer to birth registry in the SURVEY_ID table(if applicable) for identifying pregnancy
+########################################################################################################################
 #example meanings_birth_registry<-c("birth_registry", "birth_registry_meanings")
 #keep in mind this records will be classified as end_of_pregnancy so no spontaneous abortion registry should be included
 meanings_birth_registry<-c("birth_registry_mother")
-#######################################################################################################
+########################################################################################################################
+#Specify all variables of interest to generate the Lifestyle report
+########################################################################################################################
+#Variables of interest:Smoking, Folic acid use, Alcohol abuse, BMI, SES
+#1.Identify the CDM table you used to save the information about the variables of interest.
+#2.Identify the original name of the variable of interest.
+#3.Use the information above to complete the list below.
+#4.CDM_table:name of the CDM table where you saved the information.
+#5.CDM_column: name of the CDM column where you saved the information about the name of the variable of interest.
+#6.value: name of the original variable.
+#7.c.voc: name of the CDM column where you saved the unit representing the variable of interest.
+#8.v.voc: the vocabulary used for the variable of interest.
+#9.v.date: name of the CDM column which saves the date of recording.
+#10.If you don't have information about a variable then delete that section.
+
+#example BMI(saved in MEDICAL_OBSERVATIONS, original name: body_mass_index, unit:kg/m2)
+#  BMI = list(
+#    CDM_table = "MEDICAL_OBSERVATIONS",
+#    CDM_column = "mo_source_column",
+#    value = "body_mass_index",
+#    c.voc = "mo_unit",
+#    v.voc = "kg/m2",
+#    v.date = "mo_date"
+#  )
+
+Lifestyle <- list(
+  Smoking = list(
+    CDM_table = "",
+    CDM_column = "",
+    value = "",
+    c.voc = "",
+    v.voc = "",
+    v.date = ""
+  ),
+  Folic_acid = list(
+    CDM_table = "",
+    CDM_column = "",
+    value = "",
+    c.voc = "",
+    v.voc = "",
+    v.date = ""
+  ),
+  Alcohol = list(
+    CDM_table = "",
+    CDM_column = "",
+    value = "",
+    c.voc = "",
+    v.voc = "",
+    v.date = ""
+  ),
+  BMI = list(
+    CDM_table = "",
+    CDM_column = "",
+    value = c(""),
+    c.voc = "" ,
+    v.voc = "",
+    v.date = ""
+  ),
+  SES = list(
+    CDM_table = "",
+    CDM_column = "",
+    value = c(""),
+    c.voc = "" ,
+    v.voc = "",
+    v.date = ""
+  )
+)
+###############################################
 
 source("packages.R")
 source("99_path.R")
 source(paste0(pre_dir, "info.R"))
+source(paste0(pre_dir,"study_parameters.R"))
 setwd(projectFolder)
-
 
 #################################################
 #Study_source_population
