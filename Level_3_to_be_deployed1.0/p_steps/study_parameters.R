7eac59d3b8483d97b90211c5d97e8e151adf6df6
Vjola Hoxhaj <vjolahoxhaj19@gmail.com>
2021-08-26 12:32:11 +0200

    Create study_parameters.R

---
+++ Level_3_to_be_deployed1.0/p_steps/study_parameters.R
@@ -0,0 +1,12 @@
+###################################################
+#Study Parameters
+#################################################
+#females age
+min_age_preg<-12
+max_age_preg<-55
+#Set parameters basic parameters
+start_study_date <- "19950101"
+end_study_date <- "20211231"
+lookback_period <- 365
+Age_max <- 56
+Age_min <- 0
