ebacdd7356e426cecb95b1a69e61ccc8d5aefa6e
Vjola Hoxhaj <vjolahoxhaj19@gmail.com>
2022-03-22 11:52:13 +0100

    Recurrent event analysis made optional, update torun file

--- Level_3_to_be_deployed1.0/to_run.R
+++ Level_3_to_be_deployed1.0/to_run.R
@@ -211,19 +211,31 @@ setwd(projectFolder)
 source("packages.R")
 source("99_path.R")
 study_name_codelist<-NULL
+recurrent_event_analysis<-"Yes"
 load(paste0(g_intermediate,"environment.RData"))
 Rmd_DIAGNOSES<-paste0(pre_dir,"/Report_10_DIAGNOSES_L3.Rmd")
 system.time(source(paste0(pre_dir,"Step_10_00_DIAGNOSES_L3.R")))
 
+
 if(sum(length(actual_tables$EVENTS),length(actual_tables$MEDICAL_OBSERVATIONS),length(actual_tables$SURVEY_OBSERVATIONS))>0){
   if(subpopulations_present=="No"){
-    system.time(render(Rmd_DIAGNOSES, output_dir = paste0(output_dir,"DIAGNOSES/"), output_file = "DIAGNOSES_L3.html")) 
+    system.time(render(paste0(pre_dir,"/Report_10_DIAGNOSES_Overview_Completeness_L3.Rmd"), output_dir = paste0(output_dir,"DIAGNOSES/"), output_file = "DIAGNOSES_Overview_Completeness_L3.html")) 
+    system.time(render(paste0(pre_dir,"/Report_10_DIAGNOSES_Counts_L3.Rmd"), output_dir = paste0(output_dir,"DIAGNOSES/"), output_file = "DIAGNOSES_Counts_L3.html")) 
+    system.time(render(paste0(pre_dir,"/Report_10_DIAGNOSES_Rates_L3.Rmd"), output_dir = paste0(output_dir,"DIAGNOSES/"), output_file = "DIAGNOSES_Rates_L3.html")) 
+    
   } else {
     for (a in 1: length(subpopulations_names)){
-      system.time(render(Rmd_DIAGNOSES, output_dir = paste0(output_dir,"DIAGNOSES/"), output_file = paste0(subpopulations_names[a],"_DIAGNOSES_L3.html")))  
+      system.time(render(paste0(pre_dir,"/Report_10_DIAGNOSES_Overview_Completeness_L3.Rmd"), output_dir = paste0(output_dir,"DIAGNOSES/"), output_file = paste0(subpopulations_names[a],"_DIAGNOSES_Overview_Completeness_L3.html")))  
+    }
+    for (a in 1: length(subpopulations_names)){
+      system.time(render(paste0(pre_dir,"/Report_10_DIAGNOSES_Counts_L3.Rmd"), output_dir = paste0(output_dir,"DIAGNOSES/"), output_file = paste0(subpopulations_names[a],"_DIAGNOSES_Counts_L3.html")))  
+    }
+    for (a in 1: length(subpopulations_names)){
+      system.time(render(paste0(pre_dir,"/Report_10_DIAGNOSES_Rates_L3.Rmd"), output_dir = paste0(output_dir,"DIAGNOSES/"), output_file = paste0(subpopulations_names[a],"_DIAGNOSES_Rates_L3.html")))  
     }
   }
 }
+
 source(paste0(pre_dir,"save_environment.R"))
 #################################################
 #Pregnancy
